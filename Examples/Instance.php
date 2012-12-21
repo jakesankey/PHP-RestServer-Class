a0aa7b28594b6a02d0c315b9e9046086197443fa
Jake Sankey <jakesankey@icloud.com>
2012-12-20 23:41:10 -0600

    Added an addtional example method.
    Added addInts($n1, $n2) method.
    /Instance.php?method=addInts&n1=5&n2=3

--- Examples/Instance.php
+++ Examples/Instance.php
@@ -1,14 +1,26 @@
 <?php
 
-  // example request: http://path/to/Examples/Instance.php?method=sayHello&name=World
-
   require_once "RestServer.php";
 
   class Hello
   {
+     // example: http://path/to/Examples/Instance.php?method=sayHello&name=World
      public function sayHello($name)
      {
         return array("Response" => "Hello, " . $name);
+     }
+
+     // example: http://path/to/Examples/Instance.php?method=addInts&n1=3&n2=5
+     public function addInts($n1, $n2)
+     {
+        if (is_numeric($n1) && is_numeric($n2))
+        {
+          return array("Result" => "$n1 + $n2 = " . (string)($n1 + $n2));
+        }
+        else
+        {
+          return array("Error" => "Parameters must be numeric.");
+        }
      }
   }
 
